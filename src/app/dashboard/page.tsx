c52e0824bb8194712886b162287d49d0f0ef129d
Nagadhatri <nagadhatritirumalasetty@gmail.com>
2026-07-07 15:16:53 +0530

    Update dashboard layout: Move Pending Requests and ChatPanel to left, delete Profile panel

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,27 +4,27 @@ import BookingRequestsPanel from "@/components/owner/BookingRequestsPanel";
 import BookingHistory from "@/components/owner/BookingHistory";
 import { DashboardProvider } from "@/components/owner/DashboardContext";
 import ChatPanel from "@/components/chatbot/ChatPanel";
-import ProfileAndConnectionsPanel from "@/components/owner/ProfileAndConnectionsPanel";
 
 export default function DashboardPage() {
   return (
     <DashboardProvider>
       <div className="p-6">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-          <div className="lg:col-span-3 space-y-6">
+          <div className="lg:col-span-4 space-y-6">
             <CalendarView />
-            <ProfileAndConnectionsPanel />
+            <div className="h-[500px]">
+              <ChatPanel context="owner" mode="inline" />
+            </div>
+            <BookingRequestsPanel />
           </div>
           <div className="lg:col-span-5 space-y-6">
             <ScheduleManagement />
           </div>
-          <div className="lg:col-span-4 space-y-6">
-            <BookingRequestsPanel />
+          <div className="lg:col-span-3 space-y-6">
             <BookingHistory />
           </div>
         </div>
       </div>
-      <ChatPanel context="owner" />
     </DashboardProvider>
   );
 }
